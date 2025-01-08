b3f52fd1bf6868a3badb7d690be1d546b86d39ea
MahaOfcl14 <itzmahaarun@gmail.com>
2025-01-08 14:05:09 +0530

    Created UI for Categories and Advantages

--- vayuratha/src/main.jsx
+++ vayuratha/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppLayout from './AppLayout.jsx'
-import Categories from './component/Categories/Categories.jsx'
-
+import Categories from './pages/Categories'
+import Advantages from './pages/Advantages'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppLayout />
-    <Categories/>
-    
+   
+   <Categories/>
+   <Advantages/>
   </StrictMode>,
 )
---
+++ vayuratha/src/pages/Advantages.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Advantages = () => {
+  return (
+    <div className='2xl:container mx-auto'>
+  <div class="w-[90%] md:w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 py-5 gap-2 md:hover:cursor-pointer">
+               {/*Card-1*/}
+ 
+                <div class="bg-slate-100 flex flex-col items-start text-center p-6 rounded-xl">
+                    <h1 class="font-[Montserrat] text-left font-medium text-[28px] text-black">Modify Easily - Any Time, Anywhere</h1>
+
+                    <p class="font-[Montserrat] font-normal text-[14px] text-left text-black pt-3">Experience smooth, stable footage with our Advanced EIS technology for professional-grade video quality and seamless flight.</p>
+                        <img src="https://freepngimg.com/thumb/technology/49670-9-cctv-camera-image-free-transparent-image-hd.png" alt="cam-image" className='h-[100px] items-end' />
+
+                </div>
+{/*Card-1*/}
+ 
+<div class="bg-slate-100 flex flex-col items-start text-center p-6 rounded-xl">
+<h1 class="font-[Montserrat] text-left font-medium text-[28px] text-black">Modify Easily - Any Time, Anywhere</h1>
+
+                    <p class="font-[Montserrat] font-normal text-[14px] text-left text-black pt-3">Experience smooth, stable footage with our Advanced EIS technology for professional-grade video quality and seamless flight.</p>
+                        <img src="https://freepngimg.com/thumb/technology/49670-9-cctv-camera-image-free-transparent-image-hd.png" alt="cam-image" className='h-[100px] items-end' />
+
+                </div>
+                {/*Card-3*/}
+ 
+                <div class="bg-slate-100 flex flex-col items-start text-center p-6 rounded-xl">
+                <h1 class="font-[Montserrat] text-left font-medium text-[28px] text-black">Modify Easily - Any Time, Anywhere</h1>
+
+                    <p class="font-[Montserrat] font-normal text-[14px] text-left text-black pt-3">Experience smooth, stable footage with our Advanced EIS technology for professional-grade video quality and seamless flight.</p>
+                        <img src="https://freepngimg.com/thumb/technology/49670-9-cctv-camera-image-free-transparent-image-hd.png" alt="cam-image" className='h-[100px] items-end' />
+
+                </div>
+
+                </div>
+
+
+
+    </div>
+  )
+}
+
+export default Advantages
